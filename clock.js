8fcf02fd84aee892fbaf2bebfb5846b33c2f6fdc
vignesh m <vingesh820@gmail.com>
2024-09-27 16:18:15 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,10 @@
+function updateclock(){
+     const now =new Date();
+     const hours=String(now.getHours()).padStart(2,'0');
+     const minutes=String(now.getMinutes()).padStart(2,'0');
+     const seconds=String(now.getSeconds()).padStart(2,'0');
+     const timestring=`${hours}:${minutes}:${seconds}`;
+     document.getElementById('time').textContent=timestring;
+}
+setInterval(updateclock,1000);
+updateclock();
